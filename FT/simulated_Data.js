0932415c710438e5f2a766b9eb3407c477d1ea67
ayado7 <aya.douba@code.berlin>
2020-04-06 12:35:01 +0200

    new file:   JS-Functions.txt 	modified:   index.html 	modified:   logic.js 	modified:   post.html 	modified:   profile.html 	modified:   sign-in.html 	modified:   styles.css

---
+++ FT/simulated_Data.js
@@ -0,0 +1,52 @@
+/* 
+* This file contains dummy data that can be used to test without a server connection.
+* Authors: 
+*      Frank Trollmann: Initial setup of the file and creation of initial dummy data
+*      Aya Douba:
+*/
+
+
+var postCounter = 5;
+
+var user_aya = {
+    "userId":1,
+    "name":"Aya",
+    "password":"Test1234",
+    "mail":"aya.douba@code.berlin"
+};
+
+var user_frank = {
+    "userId":2,
+    "name":"Frank",
+    "password":"BLA",
+    "mail":"frank.trollmann@code.berlin"
+};
+
+var post1 = {
+    "postId":1,
+    "text":"This"
+    // this is your job!
+};
+
+var post2 = {
+    "postId":2,
+    "text":"is"
+    // this is your job!
+};
+
+var post3 = {
+    "postId" : 3,
+    "text":"your"
+    // this is your job
+};
+
+var post4 = {
+    "postId" : 4,
+    "text":"job!"
+    // this is your job
+};
+
+// post lists
+var all_posts = [post1, post2, post3, post4];
+var aya_posts = [post1, post3, post4];
+var frank_posts = [post2];
---
+++ logic.js
@@ -0,0 +1,23 @@
+alert("Hello World"); //popup
+
+let age = 28 //let allows reassignment
+const age = 30 //const doesn't allow. Use it mostly
+
+const string =(`my name is ${age}`)//to insert value into sting
+//listJSON = JSON.stringify(list) <--to send to the server
+
+
+/*
+For +1 likes
+<i class="add-like"></i> <a href="#" onclick="AddValue()">0</a>
+
+<script>
+var startingNumber = 0;
+
+function AddValue()
+{
+   startingNumber++;
+   $(this).text(startingNumber);
+}
+
+</script>*/
